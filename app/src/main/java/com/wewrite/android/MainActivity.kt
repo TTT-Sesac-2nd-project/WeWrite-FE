599487e4d3599f453d10908bdef95ab7999bbe47
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-01-19 15:57:57 +0900

    기초 navi Setting

--- app/src/main/java/com/wewrite/android/MainActivity.kt
+++ app/src/main/java/com/wewrite/android/MainActivity.kt
@@ -2,10 +2,45 @@ package com.wewrite.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wewrite.android.databinding.ActivityMainBinding
+import com.wewrite.android.group.GroupFragment
+import com.wewrite.android.home.HomeFragment
+import com.wewrite.android.map.MapFragment
+import com.wewrite.android.myPage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // 뷰 바인딩
+        setContentView(binding.root)
+
+        binding.bottomNavi.run { setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.navi_home -> {
+                    // 다른 프래그먼트 화면으로 이동하는 기능
+                    val homeFragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, homeFragment).commit()
+                }
+                R.id.navi_map -> {
+                    val boardFragment = MapFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, boardFragment).commit()
+                }
+                R.id.navi_group -> {
+                    val settingFragment = GroupFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, settingFragment).commit()
+                }
+                R.id.navi_mypage -> {
+                    val settingFragment = MyPageFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, settingFragment).commit()
+                }
+            }
+            true
+        }
+            selectedItemId = R.id.navi_home
+        }
     }
+
+    //네
 }
---
+++ app/src/main/java/com/wewrite/android/group/GroupFragment.kt
@@ -0,0 +1,26 @@
+package com.wewrite.android.group
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wewrite.android.R
+
+class GroupFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group, container, false)
+    }
+
+}
