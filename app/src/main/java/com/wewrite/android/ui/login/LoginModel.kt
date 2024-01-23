c6f3321bb2000f158071bbaea67fee705e4c9655
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-01-23 20:47:44 +0900

    자동 로그인을 위한 LoginModel 생성 및 function 분리, 자동 로그인 로직 생성

--- app/src/main/java/com/wewrite/android/ui/login/LoginModel.kt
+++ app/src/main/java/com/wewrite/android/ui/login/LoginModel.kt
@@ -1,7 +1,12 @@
 package com.wewrite.android.api.data.com.wewrite.android.ui.login
 
+import android.app.Activity
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.wewrite.android.ui.MainActivity
+import com.wewrite.android.ui.login.LoginActivity
 
 class LoginModel(private val context: Context){
     fun saveToken(token: String) {
@@ -14,5 +19,16 @@ class LoginModel(private val context: Context){
     fun getToken(): String {
         val pref = context.getSharedPreferences("pref", MODE_PRIVATE)
         return pref.getString("accessToken", "").toString()
+    }
+
+    fun goToLoginActivity(nowActivity: Activity) {
+        val intent = Intent(nowActivity, LoginActivity::class.java)
+        nowActivity.startActivity(intent)
+    }
+
+    //토큰 확인 성공시 메인 화면으로 가는 함수
+    fun goToMainActivity(nowActivity: Activity) {
+        val intent = Intent(nowActivity, MainActivity::class.java)
+        nowActivity.startActivity(intent)
     }
 }
