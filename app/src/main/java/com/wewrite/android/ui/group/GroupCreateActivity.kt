a7b02628b8b47263585818636547d4a4af25856e
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-01-27 19:25:29 +0900

    createWrite, groupCreate 각각 이미지 적용 로직 추가

--- app/src/main/java/com/wewrite/android/ui/group/GroupCreateActivity.kt
+++ app/src/main/java/com/wewrite/android/ui/group/GroupCreateActivity.kt
@@ -1,16 +1,48 @@
 package com.wewrite.android.ui.group
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.wewrite.android.R
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.wewrite.android.databinding.ActivityGroupCreateBinding
+import com.wewrite.android.util.PermissionCheck
 
 private lateinit var binding: ActivityGroupCreateBinding
 
 class GroupCreateActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                PermissionCheck.openGallery(this, setImageResult)
+            } else {
+                // 권한이 거부된 경우
+                PermissionCheck.showPermissionDeniedDialog(this)
+            }
+        }
+
+    private val setImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            data?.data?.let {
+                binding.ivGroupImage.setImageURI(it)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGroupCreateBinding.inflate(layoutInflater)
+        setImagePlusButton()
         setContentView(binding.root)
+    }
+
+    private fun setImagePlusButton() {
+        binding.clGroupImage.setOnClickListener {
+            PermissionCheck.requestPermissionAndOpenGallery(
+                this,
+                requestPermissionLauncher,
+                setImageResult
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/wewrite/android/util/PermissionCheck.kt
@@ -0,0 +1,69 @@
+package com.wewrite.android.util
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.Fragment
+
+const val PERMISSION_CODE = 101
+
+class PermissionCheck {
+    companion object {
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        fun requestPermissionAndOpenGallery(
+            activity: FragmentActivity,
+            requestPermissionLauncher: ActivityResultLauncher<String>,
+            setImageResult: ActivityResultLauncher<Intent>
+        ) {
+            val permission = Manifest.permission.READ_MEDIA_IMAGES
+
+            // 권한이 이미 허용되어 있는지 확인
+            when {
+                ContextCompat.checkSelfPermission(
+                    activity,
+                    permission
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    openGallery(activity, setImageResult)
+                }
+
+                else -> {
+                    // 권한을 요청
+                    requestPermissionLauncher.launch(permission)
+                }
+            }
+        }
+
+        fun showPermissionDeniedDialog(activity: Context) {
+            AlertDialog.Builder(activity)
+                .setMessage("권한이 거부되었습니다.")
+                .setPositiveButton("확인") { _, _ ->
+                    // 사용자가 확인 버튼을 누를 때 수행할 작업을 추가할 수 있습니다.
+                }
+                .show()
+        }
+
+        fun showPermissionDeniedDialog(fragment: Fragment) {
+            AlertDialog.Builder(fragment.requireContext())
+                .setMessage("권한이 거부되었습니다. 갤러리를 열 수 없습니다.")
+                .setPositiveButton("확인") { _, _ ->
+                    // 사용자가 확인 버튼을 누를 때 수행할 작업을 추가할 수 있습니다.
+                }
+                .show()
+        }
+
+        fun openGallery(activity: FragmentActivity, setImageResult: ActivityResultLauncher<Intent>) {
+            Log.d("Gallery", "Opening gallery...")
+            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            setImageResult.launch(galleryIntent)
+        }
+    }
+}
