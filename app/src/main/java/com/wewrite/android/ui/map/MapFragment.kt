876f2af51a7b5b787246064b0cfb2e28db1d0c5c
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-01-27 17:34:23 +0900

    카카오맵 테스트 및 확인

--- app/src/main/java/com/wewrite/android/ui/map/MapFragment.kt
+++ app/src/main/java/com/wewrite/android/ui/map/MapFragment.kt
@@ -5,21 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.wewrite.android.R
+import com.wewrite.android.databinding.FragmentMapBinding
+import net.daum.mf.map.api.MapView
 
 class MapFragment : Fragment() {
 
+    private lateinit var binding: FragmentMapBinding
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+    ): View {
+        binding = FragmentMapBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupMapContainer()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupMapContainer() {
+        mapView = MapView(requireContext())
+        binding.mapContainer.addView(mapView)
+    }
+
+
 }
